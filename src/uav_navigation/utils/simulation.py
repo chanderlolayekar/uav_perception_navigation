dda78aa5c4f00422026659d6eca98f48c14dd097
chanderlolayekar <clolayekar@gmail.com>
2026-09-04 12:28:25 -0700

    Update simulation.py

--- src/uav_navigation/utils/simulation.py
+++ src/uav_navigation/utils/simulation.py
@@ -1,39 +1,39 @@
-import rospy [cite: 335]
-from nav_msgs.msg import OccupancyGrid [cite: 335]
-from geometry_msgs.msg import PoseStamped [cite: 335]
-from sensor_msgs.msg import PointCloud2 [cite: 335]
-import csv [cite: 335]
-import numpy as np [cite: 335]
+import rospy 
+from nav_msgs.msg import OccupancyGrid 
+from geometry_msgs.msg import PoseStamped 
+from sensor_msgs.msg import PointCloud2 
+import csv 
+import numpy as np 
 
-class SimulationInterface: [cite: 335]
-    def __init__(self): [cite: 335]
-        rospy.init_node('uav_interface', anonymous=True) [cite: 335]
-        self.map_data = None [cite: 335]
-        self.pose = None [cite: 335]
-        self.pointcloud = None [cite: 335, 336]
-        rospy.Subscriber('/esdf_map', OccupancyGrid, self.map_callback) [cite: 336]
-        rospy.Subscriber('/uav_pose', PoseStamped, self.pose_callback) [cite: 336]
-        rospy.Subscriber('/lidar_points', PointCloud2, self.pc_callback) [cite: 336]
+class SimulationInterface: 
+    def __init__(self): 
+        rospy.init_node('uav_interface', anonymous=True) 
+        self.map_data = None 
+        self.pose = None 
+        self.pointcloud = None 
+        rospy.Subscriber('/esdf_map', OccupancyGrid, self.map_callback) 
+        rospy.Subscriber('/uav_pose', PoseStamped, self.pose_callback) 
+        rospy.Subscriber('/lidar_points', PointCloud2, self.pc_callback) 
 
-    def map_callback(self, msg): [cite: 336]
-        self.map_data = msg [cite: 336]
+    def map_callback(self, msg): 
+        self.map_data = msg 
 
-    def pose_callback(self, msg): [cite: 337]
-        self.pose = msg [cite: 337]
+    def pose_callback(self, msg): 
+        self.pose = msg 
 
-    def pc_callback(self, msg): [cite: 337]
-        self.pointcloud = msg [cite: 337]
+    def pc_callback(self, msg): 
+        self.pointcloud = msg 
 
-class DataLogger: [cite: 339]
-    def __init__(self, filename='uav_log.csv'): [cite: 340]
-        self.filename = filename [cite: 340]
-        with open(self.filename, 'w', newline='') as csvfile: [cite: 340]
-            writer = csv.writer(csvfile) [cite: 340]
-            writer.writerow(['timestamp', 'x', 'y', 'z', 'vx', 'vy', 'vz']) [cite: 340]
+class DataLogger: 
+    def __init__(self, filename='uav_log.csv'): 
+        self.filename = filename 
+        with open(self.filename, 'w', newline='') as csvfile: 
+            writer = csv.writer(csvfile) 
+            writer.writerow(['timestamp', 'x', 'y', 'z', 'vx', 'vy', 'vz']) 
 
-    def log_pose(self, pose_stamped): [cite: 340]
-        pos = pose_stamped.pose.position [cite: 340]
-        vx, vy, vz = 0.0, 0.0, 0.0 [cite: 341]
-        with open(self.filename, 'a', newline='') as csvfile: [cite: 341]
-            writer = csv.writer(csvfile) [cite: 341]
-            writer.writerow([pose_stamped.header.stamp.to_sec(), pos.x, pos.y, pos.z, vx, vy, vz]) [cite: 341]
+    def log_pose(self, pose_stamped): 
+        pos = pose_stamped.pose.position 
+        vx, vy, vz = 0.0, 0.0, 0.0 
+        with open(self.filename, 'a', newline='') as csvfile: 
+            writer = csv.writer(csvfile) 
+            writer.writerow([pose_stamped.header.stamp.to_sec(), pos.x, pos.y, pos.z, vx, vy, vz]) 
