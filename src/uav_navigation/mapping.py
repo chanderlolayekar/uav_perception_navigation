8a86cafbdbc47d2e2ab132d67d970b1859697dc1
chanderlolayekar <clolayekar@gmail.com>
2026-09-04 12:22:01 -0700

    Update mapping.py

--- src/uav_navigation/mapping.py
+++ src/uav_navigation/mapping.py
@@ -1,46 +1,46 @@
 import numpy as np
-from fiesta import EsdfServer, EsdfIntegrator # Hypothetical python bindings for FIESTA [cite: 11, 12]
-import sensor_msgs.point_cloud2 as pc2 # For ROS point cloud processing [cite: 13]
+from fiesta import EsdfServer, EsdfIntegrator # Hypothetical python bindings for FIESTA 
+import sensor_msgs.point_cloud2 as pc2 # For ROS point cloud processing 
 import rospy
-from sensor_msgs.msg import PointCloud2 [cite: 14, 15]
+from sensor_msgs.msg import PointCloud2 
 
-class EsdfMappingNode: [cite: 16]
-    def __init__(self): [cite: 17]
+class EsdfMappingNode: 
+    def __init__(self): 
         # Initialize ROS node
-        rospy.init_node('esdf_mapping_node') [cite: 18, 19]
+        rospy.init_node('esdf_mapping_node') 
         # Initialize ESDF Server with parameters
-        self.esdf_server = EsdfServer(resolution=0.1, map_size=50) # 10cm voxel, 50m cubical [cite: 23, 24, 25]
+        self.esdf_server = EsdfServer(resolution=0.1, map_size=50) # 10cm voxel, 50m cubical 
         # Point cloud subscriber from simulated LIDAR
-        self.pointcloud_sub = rospy.Subscriber('/lidar_points', PointCloud2, self.pointcloud_callback) [cite: 26, 27, 28, 29]
+        self.pointcloud_sub = rospy.Subscriber('/lidar_points', PointCloud2, self.pointcloud_callback) 
         # Store latest point cloud for processing in callback
-        self.latest_points = None [cite: 30, 31]
+        self.latest_points = None 
 
-    def pointcloud_callback(self, msg): [cite: 32]
+    def pointcloud_callback(self, msg): 
         # Convert ROS PointCloud2 message to numpy array of points
-        points = [] [cite: 33, 34]
-        for p in pc2.read_points(msg, skip_nans=True): [cite: 35]
-            points.append([p[0], p[1], p[2]]) [cite: 36]
-        self.latest_points = np.array(points) [cite: 37]
+        points = [] 
+        for p in pc2.read_points(msg, skip_nans=True):
+            points.append([p[0], p[1], p[2]]) 
+        self.latest_points = np.array(points) 
 
-    def update_esdf_map(self): [cite: 38]
-        if self.latest_points is None: [cite: 39]
-            return [cite: 40]
+    def update_esdf_map(self): 
+        if self.latest_points is None: 
+            return 
         # Integrate the latest point cloud into the ESDF map
-        self.esdf_server.integrate_pointcloud(self.latest_points) [cite: 41]
+        self.esdf_server.integrate_pointcloud(self.latest_points) 
         # Update ESDF distance field incrementally
-        self.esdf_server.update_esdf() [cite: 42, 43]
+        self.esdf_server.update_esdf() 
 
-    def get_esdf_distance(self, position): [cite: 44]
+    def get_esdf_distance(self, position): 
         # Query distance to nearest obstacle at given position
-        return self.esdf_server.get_distance_at(position) [cite: 45, 46]
+        return self.esdf_server.get_distance_at(position)
 
-    def spin(self): [cite: 47]
-        rate = rospy.Rate(10) # 10 Hz update rate [cite: 48, 49]
-        while not rospy.is_shutdown(): [cite: 50]
-            self.update_esdf_map() [cite: 51]
-            rate.sleep() [cite: 52]
+    def spin(self): 
+        rate = rospy.Rate(10) # 10 Hz update rate 
+        while not rospy.is_shutdown(): 
+            self.update_esdf_map() 
+            rate.sleep() 
 
-if __name__ == "__main__": [cite: 54, 55, 56, 168]
-    esdf_node = EsdfMappingNode() [cite: 57, 58]
-    print("Starting ESDF mapping node...") [cite: 59]
-    esdf_node.spin() [cite: 60]
+if __name__ == "__main__":
+    esdf_node = EsdfMappingNode() 
+    print("Starting ESDF mapping node...") 
+    esdf_node.spin() 
